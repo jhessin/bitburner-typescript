dd6eda02ca26c20310d9e2f9eefb17689be63fac
Jim Hessin <jhessin@gmail.com>
2023-06-27 15:40:07 -0600

    Fixed errors.

--- src/main/corp/marketTa.js
+++ src/main/corp/marketTa.js
@@ -1,25 +1,28 @@
-
 // const mat = warehouse.materials[matName];
 // const businessFactor = this.getBusinessFactor(office); //Business employee productivity
-      const businessProd = 1 + office.employeeProd[EmployeePositions.Business];
-      return calculateEffectWithFactors(n: businessProd, expFac: 0.26, linearFac: 10e3);
-          // -- exp must be > 0 && < 1
-          // -- linearFac must be > 1
-          // employeeProd is calculateProductivity for each employee, and is the hardest
-          // * look in Employee.ts - line 88, each takes employee value + multipliers
-          //    for corporation upgrades and industry research
-          //    * Base is mor * hap * ene * 1e-6
-          //    * Multipliers added for stat ratios for each position, i.e. 
-                    // case EmployeePositions.Operations:
-                    //   prodMult = 0.6 * effInt + 0.1 * effCha + this.exp + 0.5 * effCre + effEff;
+const businessProd = 1 + office.employeeProd[EmployeePositions.Business];
+return calculateEffectWithFactors({
+  n: businessProd,
+  expFac: 0.26,
+  linearFac: 10e3,
+});
+// -- exp must be > 0 && < 1
+// -- linearFac must be > 1
+// employeeProd is calculateProductivity for each employee, and is the hardest
+// * look in Employee.ts - line 88, each takes employee value + multipliers
+//    for corporation upgrades and industry research
+//    * Base is mor * hap * ene * 1e-6
+//    * Multipliers added for stat ratios for each position, i.e.
+// case EmployeePositions.Operations:
+//   prodMult = 0.6 * effInt + 0.1 * effCha + this.exp + 0.5 * effCre + effEff;
 //            | Cha | Cre | Int | Eff | exp |
-// Position   |------------------------------ 
+// Position   |------------------------------
 // Operations | 0.1 | 0.5 | 0.6 | 1.0 | 1.0 |
 // Engineer   | 0.1 |   0 | 1.0 | 1.0 | 1.5 |
-// Business   | 1.0 |   0 | 0.4 |   0 | 0.5 | 
+// Business   | 1.0 |   0 | 0.4 |   0 | 0.5 |
 // Management | 2.0 | 1.0 |   0 | 0.7 | 1.0 |
 // RandD      |   0 | 1.0 | 1.5 | 0.5 | 0.8 |
-          // then take prodBase * multiplier to get employeeProd
+// then take prodBase * multiplier to get employeeProd
 
 // const advertisingFactor = this.getAdvertisingFactors()[0]; //Awareness + popularity
 // const marketFactor = this.getMarketFactor(mat); //Competition + demand
@@ -28,7 +31,7 @@
 
 function calculateBestMarketPrice(mat, quantity) {
   const prod = mat.prd;
-  quantity = quantity || prod
+  quantity = quantity || prod;
 
   // mat.bCost is the displayed 'Market Price' - the buy price
   // corporation.getSalesMultiplier is corporation..upgradeMultipliers[8]; -- from sales bots?
@@ -54,7 +57,9 @@ function calculateBestMarketPrice(mat, quantity) {
       optimalPrice = 0; // No production
     } else {
       optimalPrice = mat.bCost + markupLimit;
-      console.warn(`In Corporation, found illegal 0s when trying to calculate MarketTA2 sale cost`);
+      console.warn(
+        `In Corporation, found illegal 0s when trying to calculate MarketTA2 sale cost`,
+      );
     }
   } else {
     optimalPrice = numerator / denominator + mat.bCost;
