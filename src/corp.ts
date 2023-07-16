029066dd9cc3357ae64734c327f1c387feaff7c8
Jim Hessin <jhessin@gmail.com>
2023-07-16 12:01:49 -0600

    Streamlined corporation.

--- src/corp.ts
+++ src/corp.ts
@@ -1,19 +1,121 @@
-import type { CorpIndustryName, NS } from '@ns';
+import type { CorpIndustryName, CorpResearchName, Division, NS } from '@ns';
+
+type PurchaseAsset = () => void;
 
 const CORPORATION_NAME = 'Corpie';
+const DIVISIONS: [CorpIndustryName, string][] = [
+  ['Agriculture', 'Aggies'],
+  ['Spring Water', 'SmartWater'],
+];
+const REASEARCHES: CorpResearchName[] = [
+  'Hi-Tech R&D Laboratory',
+  'Market-TA.I',
+  'Market-TA.II',
+  'AutoBrew',
+  'AutoPartyManager',
+  'HRBuddy-Recruitment',
+  'HRBuddy-Training',
+];
 
 export async function main(ns: NS) {
-  const DIVISIONS: [CorpIndustryName, string][] = [
-    [ns.corporation.getConstants().industryNames[0], 'Aggies'],
-  ];
+  ns.disableLog('ALL');
+  let corp = ns.corporation.getCorporation();
+  let funds = corp.funds;
+  let purchases: [string, number, PurchaseAsset][] = [];
+
+  function setSmartSupply(industry: [CorpIndustryName, string]) {
+    if (!ns.corporation.hasUnlock('Smart Supply')) return;
+    const data = ns.corporation.getDivision(industry[1]);
+    const info = ns.corporation.getIndustryData(industry[0]);
+    for (const city of data.cities) {
+      ns.corporation.setSmartSupply(data.name, city, true);
+      for (const material of Object.keys(info.requiredMaterials)) {
+        ns.corporation.setSmartSupplyOption(
+          data.name,
+          city,
+          material,
+          'leftovers',
+        );
+      }
+    }
+  }
 
   function expandIndustry() {
-    const industry = DIVISIONS.pop();
-    if (industry) ns.corporation.expandIndustry(industry[0], industry[1]);
+    // if (ns.corporation.getCorporation().divisions.length < DIVISIONS.length) {
+    const industry =
+      DIVISIONS[ns.corporation.getCorporation().divisions.length];
+    const info = ns.corporation.getIndustryData(industry[0]);
+    const cost = info.startingCost;
+    purchases.push([
+      `expand into ${industry[0]}`,
+      cost,
+      () => {
+        if (industry && funds >= cost) {
+          ns.corporation.expandIndustry(industry[0], industry[1]);
+          funds -= cost;
+        }
+      },
+    ]);
+
+    // configure smart supply
+  }
+
+  function assignWorkers(div: Division) {
+    for (const city of div.cities) {
+      // assign 1 slot to each non-research job.
+      ns.corporation.setAutoJobAssignment(div.name, city, 'Operations', 1);
+      ns.corporation.setAutoJobAssignment(div.name, city, 'Engineer', 1);
+      ns.corporation.setAutoJobAssignment(div.name, city, 'Business', 1);
+      ns.corporation.setAutoJobAssignment(div.name, city, 'Management', 1);
+      ns.corporation.setAutoJobAssignment(div.name, city, 'Intern', 1);
+
+      // complete research as needed
+      let researchDone = false;
+      let currentPoints = div.researchPoints;
+      for (const research of REASEARCHES) {
+        if (ns.corporation.hasResearched(div.name, research)) continue;
+        const cost = ns.corporation.getResearchCost(div.name, research);
+        ns.print(
+          `Current Research Points @ ${div.name}: ${ns.formatNumber(
+            currentPoints,
+          )}`,
+        );
+        ns.print(`Cost to research ${research}: ${ns.formatNumber(cost)}`);
+        if (currentPoints >= cost) {
+          ns.corporation.research(div.name, research);
+          currentPoints -= cost;
+        } else {
+          break;
+        }
+        researchDone = true;
+      }
+      // if there is still research to complete assign the rest of the workers
+      // to research.
+      // otherwise we will stick them into interns
+      const employees = ns.corporation.getOffice(div.name, city).numEmployees;
+      if (researchDone) {
+        ns.corporation.setAutoJobAssignment(
+          div.name,
+          city,
+          'Intern',
+          employees - 4,
+        );
+      } else {
+        ns.corporation.setAutoJobAssignment(
+          div.name,
+          city,
+          'Research & Development',
+          employees - 5,
+        );
+      }
+    }
   }
 
   while (true) {
     await ns.sleep(500);
+    ns.clearLog();
+    ns.tail();
+    purchases = [];
 
     // Form a corporation if needed.
     if (!ns.corporation.hasCorporation()) {
@@ -21,50 +123,99 @@ export async function main(ns: NS) {
     }
 
     // buy the first division if we need to.
-    if (ns.corporation.getCorporation().divisions.length < 1) {
+    if (ns.corporation.getCorporation().divisions.length < DIVISIONS.length) {
       expandIndustry();
     }
 
-    const corp = ns.corporation.getCorporation();
-    let funds = corp.funds;
+    corp = ns.corporation.getCorporation();
+    funds = corp.funds;
 
     // Prioritize unlocks
     for (const unlock of ns.corporation.getConstants().unlockNames) {
       const cost = ns.corporation.getUnlockCost(unlock);
-      if (funds >= cost) {
-        ns.corporation.purchaseUnlock(unlock);
-        funds -= cost;
-      }
+      purchases.push([
+        `purchase unlock ${unlock}`,
+        cost,
+        () => {
+          if (funds >= cost) {
+            ns.corporation.purchaseUnlock(unlock);
+            funds -= cost;
+          }
+        },
+      ]);
     }
 
     // then loop through the different upgrades.
     for (const upgrade of ns.corporation.getConstants().upgradeNames) {
       const cost = ns.corporation.getUpgradeLevelCost(upgrade);
-      if (funds >= cost) {
-        ns.corporation.levelUpgrade(upgrade);
-        funds -= cost;
-      }
+      purchases.push([
+        `purchase upgrade ${upgrade}`,
+        cost,
+        () => {
+          if (funds >= cost) {
+            ns.corporation.levelUpgrade(upgrade);
+            funds -= cost;
+          }
+        },
+      ]);
     }
 
     // loop through divisions
     for (const divName of ns.corporation.getCorporation().divisions) {
       const div = ns.corporation.getDivision(divName);
+      setSmartSupply([div.type, div.name]);
 
       // start with advertising.
       // advertize if we can.
-      if (funds >= ns.corporation.getHireAdVertCost(divName)) {
-        funds -= ns.corporation.getHireAdVertCost(divName);
-        ns.corporation.hireAdVert(divName);
-      }
+      let cost = ns.corporation.getHireAdVertCost(divName);
+      purchases.push([
+        `advertise @ ${divName}`,
+        cost,
+        () => {
+          if (funds >= cost) {
+            ns.corporation.hireAdVert(divName);
+            funds -= cost;
+          }
+        },
+      ]);
 
       // loop through cities
       for (const city of div.cities) {
-        const cost = ns.corporation.getOfficeSizeUpgradeCost(divName, city, 3);
-        if (funds >= cost) {
-          ns.corporation.upgradeOfficeSize(divName, city, 3);
-          funds -= cost;
-        }
+        assignWorkers(div);
+
+        // Upgrade office size when we can.
+        cost = ns.corporation.getOfficeSizeUpgradeCost(divName, city, 1);
+        purchases.push([
+          `upgrade office @ ${divName}`,
+          cost,
+          () => {
+            if (funds >= cost) {
+              ns.corporation.upgradeOfficeSize(divName, city, 1);
+              funds -= cost;
+            }
+          },
+        ]);
+
+        // upgrade warehouse when we can.
+        cost = ns.corporation.getUpgradeWarehouseCost(divName, city);
+        purchases.push([
+          `upgrade warehouse @ ${divName}`,
+          cost,
+          () => {
+            if (funds >= cost) {
+              ns.corporation.upgradeWarehouse(divName, city);
+              funds -= cost;
+            }
+          },
+        ]);
       }
+    }
+
+    purchases.sort((a, b) => a[1] - b[1]);
+    ns.print(`Current funds: $${ns.formatNumber(funds)}`);
+    for (const purchase of purchases) {
+      ns.print(`Cost to ${purchase[0]}: $${ns.formatNumber(purchase[1])}`);
+      purchase[2]();
     }
   }
 }
